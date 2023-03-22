13a41211696d4f7eb07ed8d43a912ca8d77ebba0
Lily Springer <lilyspringer@hotmail.com>
2023-03-22 16:56:46 -0600

    math challenge done

--- functions.test.js
+++ functions.test.js
@@ -1,13 +1,27 @@
-const labFunctions = require('./functions')
+const labFunctions = require("./functions");
 
-test('returnTwo test', () => {
-    expect(labFunctions.returnTwo()).toBe(2)
-})
+test("returnTwo test", () => {
+  expect(labFunctions.returnTwo()).toBe(2);
+});
 
-test('greeting test', () => {
-    expect(labFunctions.greeting('Tom Bombadil')).toBe(`Hello, Tom Bombadil`)
-})
+test("greeting test", () => {
+  expect(labFunctions.greeting("Tom Bombadil")).toBe(`Hello, Tom Bombadil`);
+});
 
-test('add test', () => {
-    expect(labFunctions.add(7,6)).toBe(13)
-})
+describe("math functions", () => {
+  test("add test", () => {
+    expect(labFunctions.add(4,2)).toBe(6);
+  });
+
+  test("subtract test", () => {
+    expect(labFunctions.subtract(4,2)).toBe(2);
+  });
+
+  test("multiply test", () => {
+    expect(labFunctions.multiply(4,2)).toBe(8);
+  });
+
+  test("add divide", () => {
+    expect(labFunctions.divide(4,2)).toBe(2);
+  });
+});
